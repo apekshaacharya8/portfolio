2f7eb1c2c7f4c8319bcb345bd047919cce726a57
Apeksha <apeksha@zycus.com>
2024-04-04 17:20:48 +0530

    skill changes

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class DashBoardComponent {
   title = 'Portfolio';
+  imageList = [{
+    name : "Angular",
+    path : "assets/images/angular.webp"
+  },{
+    name: "HTML",
+    path: "assets/images/html.png"
+  },
+  {
+    name : "CSS",
+    path : "assets/images/css.png"
+  },
+  {
+    name : "Bootstrap" ,
+    path : "assets/images/boostrap.png"
+  },
+  {
+    name : "JavaScript" ,
+    path : "assets/images/js.png"
+  },
+  {
+    name : "TypeScript" ,
+    path : "assets/images/ts.png"
+  },
+  {
+    name : "Git" ,
+    path : "assets/images/git.png"
+  },
+  {
+    name : "JIRA" ,
+    path : "assets/images/jira.png"
+  }
+]
 }
